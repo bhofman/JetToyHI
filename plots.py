63aa7875bde5f38eb99c3fdaaade8ca48e8e06da
bhofman <bas.hofman@hotmail.com>
2021-02-17 17:27:57 +0100

    plots

--- plots.py
+++ plots.py
@@ -4,35 +4,45 @@ RDF = ROOT.ROOT.RDataFrame
 
 
 ########################## Importing datasets ##########################
-rootFile_1 = ROOT.TFile("~/cernbox/a0r025ghA005.root")
+rootFile_1 = ROOT.TFile("JetToyHIResultConstituentSubtraction.root")
 rootTree_1 = rootFile_1.Get("jetTree")
 RDF_1 = RDF(rootTree_1)
 
-rootFile_2 = ROOT.TFile("~/cernbox/a1r025ghA005.root")
-rootTree_2 = rootFile_2.Get("jetTree")
-RDF_2 = RDF(rootTree_2)
 
 
 ########################## Making histograms ##########################
-H1 = RDF_1.Histo1D(( "H1", "(Reconstructed - Signal) / Signal",  100, -1,1) ,"ptPull") # These are the histograms of variable bin size
+H1 = RDF_1.Histo1D(( "Transverse momentum", "Signal pT",  100, 25,250) ,"sigJetPt") # These are the histograms of variable bin size
 H1.SetStats(0)
 H1.SetLineColor(1)
 H1.SetLineWidth(1)
+#H1.Scale(1/H1.GetEntries())
+H1.GetYaxis().SetTitle("counts")
+H1.GetXaxis().SetTitle("Jet transverse momentum in GeV")
+H3 = RDF_1.Histo1D(( "Transverse momentum", "raw pT",  100, 25,250) ,"rawJetPt") # These are the histograms of variable bin size
 
-H2 = RDF_2.Histo1D(( "H1", "(Reconstructed - Signal) / Signal",  100, -1,1) ,"ptPull") # These are the histograms of variable bin size
-H2.SetStats(0)
+
+H2 = RDF_1.Histo1D(( "Mass", "Signal M",  100, 0,30) ,"sigJetM") # These are the histograms of variable bin size
+#H2.SetStats(0)
 H2.SetLineColor(2)
 H2.SetLineWidth(1)
-
+H2.GetYaxis().SetTitle("counts")
+H2.GetXaxis().SetTitle("Jet mass in GeV")
+H4 = RDF_1.Histo1D(( "Transverse momentum", "raw pT",  100, 25,250) ,"rawJetPt") # These are the histograms of variable bin size
 
 c1 = ROOT.TCanvas()
+H1.Draw()
+H3.Draw("same")
+c1.SaveAs("~/cernbox/plots/sigJetPt.png")
 
-H1.Draw("same")
-H2.Draw("same")
+c2 = ROOT.TCanvas()
+H2.Draw()
+H4.Draw("same")
+c2.SaveAs("~/cernbox/plots/sigJetM.png")
 
-#c1.SaveAs("df002_trN.png")
+
 
 ########################## Making legend ##########################
+"""
 legend = ROOT.TLegend (0.7 ,0.6 ,0.85 ,0.75)
 
 legend.SetBorderSize(0)
@@ -46,3 +56,4 @@ legend.AddEntry(H1.DrawCopy(),"a0r025")
 legend.AddEntry(H2.DrawCopy(),"a1r025")
 
 legend.Draw("same")
+"""
