38e74696897b8d1af1ab09bd612489391874829a
bhofman <72511967+bhofman@users.noreply.github.com>
2021-09-17 14:56:13 +0200

    Update EventMixer.hh

--- PU14/EventMixer.hh
+++ PU14/EventMixer.hh
@@ -1,5 +1,7 @@
-# Author: Yi Chen (FHead) https://github.com/FHead
-# https://github.com/FHead/JetToyHI/blob/49d264cc304602341e56a315f0a9dbd768016f57/PU14/EventMixer.hh
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Author: Yi Chen (FHead) https://github.com/FHead
+// https://github.com/FHead/JetToyHI/blob/49d264cc304602341e56a315f0a9dbd768016f57/PU14/EventMixer.hh
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef __EVENTMIXER_HH__
 #define __EVENTMIXER_HH__
