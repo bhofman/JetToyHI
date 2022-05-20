eb5a2ddeb413d7b7b0dfbbb30d089845367f6d52
Bas Hofman <bashof@stbc-i1.nikhef.nl>
2022-05-20 13:50:40 +0200

    Added Dynamical Jet Charge

---
+++ include/jetChargeDynamical.hh
@@ -0,0 +1,47 @@
+#ifndef __JetChargeDynamical_HH__
+#define __JetChargeDynamical_HH__
+
+//------------------------------------------------------------------------
+/// Dynamical jet charge
+///
+/// This is defined as in https://arxiv.org/pdf/2101.04304.pdf
+
+class JetChargeDynamical {
+public:
+  /// default ctor
+  JetChargeDynamical(double Xi = 0.2, double kappaLower = 0.5,double kappaUpper = 0.5, double ptmin = -1.) :
+    _Xi(Xi),
+    _kappaLower(kappaLower),
+    _kappaHigher(kappaHigher),
+    _ptmin(ptmin)
+   {}
+
+  /// compute the function
+  virtual double result(const fastjet::PseudoJet &jet) const {
+    // check the jet is appropriate for computation
+    if (!jet.has_constituents()) {
+      Printf("Jet charge calculation can only be applied on jets for which the constituents are known.");
+      return -999.;
+    }
+    vector<fastjet::PseudoJet> constits = jet.constituents();
+    double sumJetCharge = 0;
+    double jetPt = jet.perp();
+
+    for(fastjet::PseudoJet p : constits) {
+      if(p.perp()<_ptmin) continue;
+      const double & ch = p.user_info<PU14>().charge(); //three_charge()
+      double zFrac = p.perp()/jetPt;
+      if(zFrac <= Xi) sumcharge += ch*std::pow(zFrac,_kappaLower);
+      if(zFrac > Xi) sumcharge += ch*std::pow(zFrac,_kappaHigher);
+    }
+    return sumcharge;
+  }
+  
+protected:
+  double _Xi;
+  double _kappaLower;
+  double _kappaHigher;
+  double _ptmin;
+};
+
+#endif
