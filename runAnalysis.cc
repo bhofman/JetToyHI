c0d644c9aed283ac3c5dbc0f3e338089bc5c8fc9
Bas Hofman <bas.hofman@cern.ch>
2023-02-12 14:56:59 +0100

    Updated analysis

--- runAnalysis.cc
+++ runAnalysis.cc
@@ -31,6 +31,8 @@ int main (int argc, char ** argv) {
   int nEvent = cmdline.value<int>("-nev",1);  // first argument: command line option; second argument: default value
   //bool verbose = cmdline.present("-verbose");
 
+  double R = cmdline.value<double>("-R",0.);
+
   cout << "will run on " << nEvent << " events" << endl;
 
   // Uncomment to silence fastjet banner
@@ -40,14 +42,13 @@ int main (int argc, char ** argv) {
   treeWriter trw("jetTree");
 
   //Jet definition
-  double ghostRapMax         = 6.0;
+  double ghostRapMax         = 1.0;
   double ghost_area          = 0.005;
   int    active_area_repeats = 1;
   GhostedAreaSpec ghost_spec(ghostRapMax, active_area_repeats, ghost_area);
   AreaDefinition area_def = AreaDefinition(active_area,ghost_spec);
-  JetDefinition jet_def(antikt_algorithm, R);
 
-  double jetRapMax = 3.0;
+  double jetRapMax = 0.5;
   Selector jet_selector = SelectorAbsEtaMax(jetRapMax);
 
   ProgressBar Bar(cout, nEvent);
@@ -75,34 +76,59 @@ int main (int argc, char ** argv) {
     //   fastsim
     //---------------------------------------------------------------------------
     AliceFastSim fastSim(particlesSig);
-    auto truth = fastSim.AliceAcceptance();
-    auto detector = fastSim.AliceDetector();
+    vector<PseudoJet> truth = fastSim.AliceAcceptance();
+    vector<PseudoJet> detector = fastSim.AliceDetector();
 
     //---------------------------------------------------------------------------
-    //   jet clustering
+    //   jet clustering of small R
     //---------------------------------------------------------------------------
-    JetDefinition jet_def(antikt_algorithm, 0.05);
+    JetDefinition jet_def_smaller(antikt_algorithm, R);
 
-    fastjet::ClusterSequenceArea sigTruth(truth, jet_def, area_def);
-    jetCollection jetCollectionSig_Truth(sorted_by_pt(jet_selector(sigTruth.inclusive_jets(10.))));
+    fastjet::ClusterSequenceArea sigTruth_smaller(truth, jet_def_smaller, area_def);
+    jetCollection jetCollectionSig_Truth_smaller(sorted_by_pt(jet_selector(sigTruth_smaller.inclusive_jets(10.))));
 
-    fastjet::ClusterSequenceArea sigDetector(detector, jet_def, area_def);
-    jetCollection jetCollectionSig_Detector(sorted_by_pt(jet_selector(sigDetector.inclusive_jets(10.))));
+    fastjet::ClusterSequenceArea sigDetector_smaller(detector, jet_def_smaller, area_def);
+    jetCollection jetCollectionSig_Detector_smaller(sorted_by_pt(jet_selector(sigDetector_smaller.inclusive_jets(10.))));
+/*
+    //match truth and detector jets
+    jetMatcher jetMatch_smaller(R);
+    jetMatch_smaller.setBaseJets(jetCollectionSig_Detector_smaller);
+    jetMatch_smaller.setTagJets(jetCollectionSig_Truth_smaller);
+    jetMatch_smaller.matchJets();
+    jetMatch_smaller.reorderedToTag(jetCollectionSig_Detector_smaller);
+*/
+    //---------------------------------------------------------------------------
+    //   jet clustering of small R
+    //---------------------------------------------------------------------------
+    JetDefinition jet_def_bigger(antikt_algorithm, R+0.05);
 
-    //match CSFull jets to signal jets
-    jetMatcher jetMatch_05(0.05);
-    jetMatch_05.setBaseJets(jetCollectionSig_Detector);
-    jetMatch_05.setTagJets(jetCollectionSig_Truth);
-    jetMatch_05.matchJets();
-    jetMatch_05.reorderedToTag(jetCollectionSig_Detector);
+    fastjet::ClusterSequenceArea sigTruth_bigger(truth, jet_def_bigger, area_def);
+    jetCollection jetCollectionSig_Truth_bigger(sorted_by_pt(jet_selector(sigTruth_bigger.inclusive_jets(10.))));
+
+    fastjet::ClusterSequenceArea sigDetector_bigger(detector, jet_def_bigger, area_def);
+    jetCollection jetCollectionSig_Detector_bigger(sorted_by_pt(jet_selector(sigDetector_bigger.inclusive_jets(10.))));
+
+    //match bigger to smaller jets
+    jetMatcher jetMatch_bigger(R+0.05);
+    jetMatch_bigger.setBaseJets(jetCollectionSig_Truth_bigger);
+    jetMatch_bigger.setTagJets(jetCollectionSig_Truth_smaller);
+    jetMatch_bigger.matchJets();
+    jetMatch_bigger.reorderedToTag(jetCollectionSig_Truth_bigger);
+
+    jetMatch_bigger.setBaseJets(jetCollectionSig_Detector_bigger);
+    jetMatch_bigger.setTagJets(jetCollectionSig_Detector_smaller);
+    jetMatch_bigger.matchJets();
+    jetMatch_bigger.reorderedToTag(jetCollectionSig_Detector_bigger);
 
     //---------------------------------------------------------------------------
     //   write tree
     //---------------------------------------------------------------------------
     trw.addCollection("eventWeight",   eventWeight);
 
-    trw.addCollection("sigJet_Truth_",        jetCollectionSig_Truth);
-    trw.addCollection("sigJet_Detector_",        jetCollectionSig_Detector);
+    trw.addCollection("sigJet_Truth_smaller",        jetCollectionSig_Truth_smaller);
+    trw.addCollection("sigJet_Truth_bigger",        jetCollectionSig_Truth_bigger);
+    trw.addCollection("sigJet_Detector_smaller",        jetCollectionSig_Detector_smaller);
+    trw.addCollection("sigJet_Detector_bigger",        jetCollectionSig_Detector_bigger);
 
     trw.fillTree();
 
