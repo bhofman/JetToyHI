9e4a15b36c0f9bd24c10478755489204e3a12ae9
Bas Hofman <bashof@stbc-i1.nikhef.nl>
2024-06-10 19:56:52 +0200

    Alice thermal full eta

--- include/thermalAlice.hh
+++ include/thermalAlice.hh
@@ -35,32 +35,38 @@ public :
     
     funcThrm_ = new TF1("funcThrm_","TMath::Power(x, [0]-1)*TMath::Exp(-x/[1])", 0.15, 200.); // gamma function
     funcThrm_->SetParNames("alpha", "beta");
-    funcThrm_->SetParameters(2.,0.4);
+    funcThrm_->SetParameters(2.,0.3675);
+    //funcThrm_->SetParameters(2.,0.4);
   }
   
   std::vector<fastjet::PseudoJet> createThermalEventAlice() {
 
     std::vector<fastjet::PseudoJet> particles;
 
-    //double meanN = 2500.;
-    //double sigmaN = 500.;
-    //std::random_device rd {};
-    //std::mt19937 gen {rd()};
-    //std::normal_distribution<> d {meanN, sigmaN}; // mean , sigma
+    double meanN = 2300.;
+    double sigmaN = 500.;
+    std::random_device rd {};
+    std::mt19937 gen {rd()};
+    std::normal_distribution<> d {meanN, sigmaN}; // mean , sigma
     
-    unsigned int Nparticles = 2500;//std::round(d(gen));
+    unsigned int Nparticles = std::round(d(gen));
 
     for(unsigned int i = 0; i<Nparticles; ++i) {
+      std::random_device rd_pt;
+      std::mt19937 gen_pt(rd_pt());
+      std::gamma_distribution<> d_pt(2, 0.3675);
+
       //pt from gamam function
-      double pt = funcThrm_->GetRandom();
+      double pt = d_pt(gen_pt);
       //random phi
-      double phimin = 0.;
-      double phimax = TMath::TwoPi();
-      double phi = gRandom->Rndm() * (phimax - phimin) + phimin;
+
+      double phi = gRandom->Rndm() * TMath::TwoPi();;
       //random rapidity
-      double rap = gRandom->Rndm() * 0.9;
+
+      double rap = gRandom->Rndm() * 1.8 - 0.9; // only positiveee need also negative
       //pion mass
-      double mass = 0.1395;
+
+      double mass = 0.1395; // pion mass
       
       fastjet::PseudoJet p4;
       p4.reset_momentum_PtYPhiM(pt,rap,phi,mass);
